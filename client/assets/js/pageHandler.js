ca2cac81fab00d4727b997d6c1cb0926c0fd547e
Senuka Bandara <am.senuka.bandara@gmail.com>
2025-03-03 09:58:23 +0530

    Create init html

--- client/assets/js/pageHandler.js
+++ client/assets/js/pageHandler.js
@@ -1,13 +1,23 @@
-class PageHandler {
+
+import { init } from "./script.js";
+export class PageHandler {
+
     static loadContent(page) {
-     fetch(page)
-         .then(response => response.text())
-         .then(data => {
-             document.getElementById('root').innerHTML = data;
-         })
-         .catch(error => {
-             console.error('Error loading content:', error);
-         });
- }
+
+        page = `/pages/${page}.html`;
+
+        fetch(page)
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('root').innerHTML = data;
+                init();
+
+            })
+            .catch(error => {
+                console.error('Error loading content:', error);
+            });
+
+
+    }
 
 }
---
+++ client/assets/js/script.js
@@ -0,0 +1,16 @@
+import { PageHandler } from "./pageHandler.js ";
+
+
+let createRoomForm;
+let enterRoomCode;
+
+function init() {
+
+
+    roomForm = document.querySelector('#createRoomForm');
+    enterRoomCode = document.querySelector('#roomCodeForm');
+
+    
+}
+
+export { init };
