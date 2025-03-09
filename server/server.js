050185648428f10131088da79672779e4674e549
Senuka Bandara <am.senuka.bandara@gmail.com>
2025-03-09 21:30:21 +0530

    Restructure server code

--- server/server.js
+++ server/server.js
@@ -3,26 +3,100 @@ const http = require("http");
 const socketIo = require("socket.io");
 
 const app = express();
-
 const server = http.createServer(app);
-
 const io = socketIo(server);
 
 app.use(express.static("../client"));
+app.use(express.json());
+
+/* SCHEMA
+users = { socketID: roomCode, ... }
+
+rooms = {
+    roomCode: {
+        gameData: {
+            currentWord: word,
+            currentDrawer: userID,
+            isActive: boolean
+        }
+    }
+}
+*/
+
+let users = {}; 
+let rooms = {}; 
+let messages = [];
+
+app.post("/create-room", (req, res) => {
+    const { roomCode, user } = req.body;
+
+    if (rooms[roomCode]) {
+        return res.send(null); // Room already exists
+    }
+
+    rooms[roomCode] = {
+        gameData: {
+            currentWord: null,
+            currentDrawer: null,
+            isActive: false
+        }
+    };
+
+    res.send({ roomCode });
+});
+
+app.post("/join-room", (req, res) => {
+    const { roomCode, user } = req.body;
+
+    if (!rooms[roomCode]) {
+        return res.send(null); // Room not found
+    }
+
+    res.send({ roomCode });
+});
 
 io.on("connection", (socket) => {
     console.log("A user connected:", socket.id);
 
+    socket.on("join-room", (roomCode) => {
+        socket.join(roomCode);
+        users[socket.id] = roomCode;
+
+        console.log(`${socket.id} joined room ${roomCode}`);
+
+        io.to(roomCode).emit("user-joined", { userId: socket.id });
+    });
+
     socket.on("draw", (data) => {
-        socket.broadcast.emit("draw", data); 
+        const roomCode = users[socket.id];
+        if (roomCode) {
+            socket.to(roomCode).emit("draw", data); 
+        }
     });
 
     socket.on("chat", (message) => {
-        io.emit("chat", message); 
+        const roomCode = users[socket.id];
+        if (roomCode) {
+            io.to(roomCode).emit("chat", message); 
+        }
     });
 
     socket.on("disconnect", () => {
         console.log("A user disconnected:", socket.id);
+
+        const roomCode = users[socket.id];
+        if (roomCode) {
+            socket.leave(roomCode);
+            const {[socket.id]: _, ...rest} = users;
+            users = rest;
+
+            // Check if room is empty
+            if (io.sockets.adapter.rooms.get(roomCode)?.size === 0) {
+                const {[roomCode]: _, ...rest} = rooms;
+                rooms = rest;
+                console.log(`Room ${roomCode} deleted`);
+            }
+        }
     });
 });
 
